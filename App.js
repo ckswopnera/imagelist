d02dda00b232337c79792d03e024ac14938ad464
sayan <ck.swopnera@gmail.com>
2022-12-13 11:04:21 +0530

    new app initiated

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export default App = () => {
+  useEffect(() => {
+    fetch("https://picsum.photos/v2/list?page=2&limit=20")
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+      .catch((e) => console.log(e));
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
